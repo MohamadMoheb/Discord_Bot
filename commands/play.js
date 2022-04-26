d62ae1b6c28acf7df3ed9069eddab3f01811807c
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-04-26 05:19:15 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -9,8 +9,8 @@ module.exports = {
  
         if (!voiceChannel) return message.channel.send('You need to be in a channel to execute this command!');
         const permissions = voiceChannel.permissionsFor(message.client.user);
-        if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
-        if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
+        if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissions');
+        if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissions');
         if (!args.length) return message.channel.send('You need to send the second argument!');
  
         const validURL = (str) =>{
