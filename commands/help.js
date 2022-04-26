1d6d1e5fa6cf3ff56532833f30243d62ed342573
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-04-26 05:54:07 +0300

    Delete help.js

--- commands/help.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'help',
-    description: 'Lists all available commands',
-    execute(message, args){
-        message.channel.send('List of all available commands:');
-    }
-}
